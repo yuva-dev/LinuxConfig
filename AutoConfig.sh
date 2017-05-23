1d9def3e95ffb706c9ff431d1bd0267bf93ba15a
yuvi1947 <yuvi1947@users.noreply.github.com>
2017-05-23 06:45:07 +0530

    Create AutoConfig.sh

---
+++ AutoConfig.sh
@@ -0,0 +1,6 @@
+timedatectl set-local-rtc 1
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo apt-get update
+sudo apt-get install grub-customizer -y
+sudo apt-get install lib32stdc++6 -y
+sudo apt-get install ntfs-config -y
