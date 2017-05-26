5151fc1fcfd61025cf5ff03535ea774a5d4af8fb
yuvi1947 <yuvi1947@users.noreply.github.com>
2017-05-26 20:07:31 +0530

    auto install code development esentials

--- AutoConfig.sh
+++ AutoConfig.sh
@@ -7,14 +7,22 @@ sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo apt-get update
 sudo apt-get install grub-customizer -y
 
+# JAVA 8 for android studio
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-set-default -y
+
 # Fix for android studio emulator
 sudo apt-get install lib32stdc++6 -y
 
 # latest ntfs drivers
 sudo apt-get install ntfs-config -y
+echo "Run ntfs-config to configure manually later"
 
 # install git
 sudo apt-get install git -y
+echo "Configure git user email/username manually later..."
 
 # install heroku
 sudo apt-get install curl -y
@@ -26,4 +34,13 @@ sudo apt-get install heroku -y
 # install node
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# install visual studio
+# Check if the installation proceedss without any hiccup
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code -y
+
 
