9b7ff4704747a03ca87861e22a932d4fefb75b7c
yuvi1947 <yuvi1947@users.noreply.github.com>
2017-06-02 17:02:22 +0530

    mongodb included in installation

--- AutoConfig.sh
+++ AutoConfig.sh
@@ -49,3 +49,12 @@ sudo apt-get install tango-icon-theme -y
 
 # Pip installer for python
 sudo apt-get install python-pip -y
+
+# install mongodb in ubuntu 16.04 base
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+# startng mongod service
+sudo service mongod start
+
