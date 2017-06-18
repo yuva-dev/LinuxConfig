12cf5754c4b089cc2476148cb34ad0a3a98d09d7
yuvi1947 <yuvi1947@users.noreply.github.com>
2017-06-18 17:21:12 +0530

    Update AutoConfig.sh

--- AutoConfig.sh
+++ AutoConfig.sh
@@ -65,4 +65,4 @@ sudo apt-get install partitionmanager -y
 echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -cs)-getdeb apps" | sudo tee /etc/apt/sources.list.d/getdeb-apps.list
 wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install pycharm
+sudo apt-get install pycharm -y
