0c1f7e56a39c1f90890f9b78b9329ae89adb93eb
yuvi1947 <yuvi1947@users.noreply.github.com>
2017-07-09 20:57:01 +0530

    Update AutoConfig.sh

--- AutoConfig.sh
+++ AutoConfig.sh
@@ -66,3 +66,6 @@ echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -cs)-getdeb apps" | sud
 wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install pycharm -y
+
+#Installing requistes for using google analytics api
+pip install --upgrade google-api-python-client
